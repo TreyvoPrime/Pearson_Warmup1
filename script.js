525a2059eb248b3c3c109a0bcff0f0fddb7fa7dc
Treyvon <trey2044@outlook.com>
2026-02-27 15:01:36 -0500

    second

--- script.js
+++ script.js
@@ -2,24 +2,27 @@ const forward = document.getElementById("Advance");
 const hidden = document.getElementById("hidden");
 const messages = document.getElementById("message");
 let sceneNumber = 1;
+
 function AdvanceScene() {
-    sceneNumber++
-    console.log(sceneNumber)
+    sceneNumber++;
+    
     const img = document.getElementById("images");
+
     if (sceneNumber === 1) {
-        img.src = "./images/forest1.jpg"
-        document.getElementById("hidden").hidden = true;
+        img.src = "./images/forest1.jpg";
+        hidden.hidden = true;
     }
     else if (sceneNumber === 2) {
-        img.src = "./images/forest2.webp"
-        messages.textContent = "You have entered the spruce tree maze."
-        document.getElementById("hidden").hidden = true;
-
+        img.src = "./images/forest2.webp";
+        messages.textContent = "You have entered the spruce tree maze.";
+        hidden.hidden = true;
     }
     else if (sceneNumber === 3) {
-        img.src = "./images/forest3.webp"
-        hidden.textContent = "You have reached the mighty observetory your journey ends here."
+        img.src = "./images/forest3.webp";
+        messages.textContent = "You have reached the mighty observatory. Your journey ends here.";
+        hidden.hidden = false;
+        forward.disable = true;
     }
-
 }
-addEventListener("click", AdvanceScene);
+
+forward.addEventListener("click", AdvanceScene);
